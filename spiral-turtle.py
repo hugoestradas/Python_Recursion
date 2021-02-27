e74d8202256e0473908cc3d4ff180191cf03ce32
hugoestradas <hugo_estradas@outlook.com>
2021-02-26 18:28:15 -0600

    Added hanoi-tower, spiral-turtle and fractal-tree recursion projects.

---
+++ spiral-turtle.py
@@ -0,0 +1,17 @@
+import turtle
+
+MAX_LENGTH = 1000
+INCREMENT = 10
+
+def draw_spiral(a_turtle, line_length):
+    if line_length > MAX_LENGTH:
+        return
+    a_turtle.forward(line_length)
+    a_turtle.right(90)
+    draw_spiral(a_turtle, line_length + INCREMENT)
+
+charlie = turtle.Turtle(shape="turtle")
+charlie.pensize(5)
+charlie.color("light blue")
+draw_spiral(charlie, 10)
+turtle.done()
