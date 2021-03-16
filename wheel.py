72536b33045d5b8a96fa43c5b92690297219cf74
Hugo Estrada S <61108175+hugoestradas@users.noreply.github.com>
2021-03-16 09:04:16 -0600

    created wheel project

---
+++ wheel.py
@@ -0,0 +1,37 @@
+import turtle
+
+screen = turtle.Screen()
+screen.tracer(0)
+t2 = turtle.Turtle()
+t2.forward(50)
+t2.setheading(90)
+t2.circle(50)
+t2.speed(2)
+t = turtle.Turtle()
+t.speed(4)
+
+def rectangle() :
+    t.penup()
+    t.forward(170)
+    t.left(90)
+    t.pendown()
+    t.forward(5)
+    t.left(90)
+    t.forward(120)
+    t.left(90)
+    t.forward(10)
+    t.left(90)
+    t.forward(120)
+    t.left(90)
+    t.forward(5)
+def windmill():
+    for i in range(4):
+        rectangle()
+        t.penup()
+        t.goto(0,0)
+        t.pendown()
+while True:
+    t.clear()
+    windmill()
+    screen.update()
+    t.left(0.2) # Reduced here
